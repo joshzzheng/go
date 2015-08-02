a4436ac9e7e74e867171526a8012b61515ee49c3
Josh Zheng <denial@gmail.com>
2015-08-02 12:49:07 -0700

    Cleaned up settings.py.

--- go/settings.py
+++ go/settings.py
@@ -4,10 +4,8 @@ DEBUG = True
 SECRET_KEY = 'temporary_secret_key' # make sure to change this
 
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-DATABASE_URL = 'postgres://localhost/go'
+
 '''
 ACCOUNT_SID = "ACd89ad7724ff0516b6d980fa1e198c678" 
 AUTH_TOKEN = "84745c9096a1f71d230627784192afe9" 
-
-HOST_DOMAIN = "offthegridjosh.herokuapp.com"
 '''
